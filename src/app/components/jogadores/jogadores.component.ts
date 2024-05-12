a5ac62ba3ba493d2191377f8f2bf9fb2d873579f
emodudo <eduradombc@gmail.com>
2024-05-12 02:11:57 -0300

    site do inter primeira versao

---
+++ src/app/components/jogadores/jogadores.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jogadores',
+  templateUrl: './jogadores.component.html',
+  styleUrl: './jogadores.component.css'
+})
+export class JogadoresComponent {
+
+}
